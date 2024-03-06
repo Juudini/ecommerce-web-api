9838dcf6ddcb19ac32f8e926d65e9cac7e555cef
Juudini <juudinidev@gmail.com>
2024-03-06 20:23:28 -0300

    feat(presentation/routes): added 'users' routes

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { ProductRoutes } from "./product";
 import { CategoryRoutes } from "./category";
+import { AuthRoutes } from "./auth";
 
 export class AppRoutes {
     static get routes(): Router {
@@ -8,6 +9,7 @@ export class AppRoutes {
 
         router.use("/api/products", ProductRoutes.routes);
         router.use("/api/categories", CategoryRoutes.routes);
+        router.use("/api/users", AuthRoutes.routes);
 
         return router;
     }
