03eefe06d6dac1b573b0b355a2f351cce42335a8
Juudini <juudinidev@gmail.com>
2024-03-11 13:15:22 -0300

    refactor(dtos/category): removed unnecessary properties

--- server/src/domain/dtos/category/category.dto.ts
+++ server/src/domain/dtos/category/category.dto.ts
@@ -1,19 +1,14 @@
-import { ProductProps } from "../../types";
-
-//Todo: hacerVALIDACIONES CON ZOD
+//Todo: Validations wiht ZOD
 export class CategoryDto {
-    private constructor(
-        public title: string,
-        public products?: ProductProps[]
-    ) {}
+    private constructor(public title: string) {}
 
     static create = (object: { [key: string]: any }): [string?, CategoryDto?] => {
-        const { title, products } = object;
+        const { title } = object;
 
         if (!title) return ["Missing title"];
         if (title.length < 3) return ["title too short"];
         if (title.length > 50) return ["title too long"];
 
-        return [undefined, new CategoryDto(title, products)];
+        return [undefined, new CategoryDto(title)];
     };
 }
